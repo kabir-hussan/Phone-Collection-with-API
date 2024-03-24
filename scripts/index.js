787e409922b57e1cd630b6159625ab1a8f674fdc
kabir-hussan <kabirhussan891@gmail.com>
2024-03-24 11:04:21 +0600

    loading spinner added

--- scripts/index.js
+++ scripts/index.js
@@ -36,13 +36,26 @@ const displayPhone = phones => {
     `;
     phoneContainer.appendChild(phoneDiv);
     });
+    toggleLoadingSpinner(false);
 }
 
 const searchBtn= ()=>{
+    toggleLoadingSpinner(true);
     const searchField= document.getElementById('search-field');
     const searchText= searchField.value;
-    console.log(searchText);
     loadPhone(searchText); 
+   
+}
+
+const toggleLoadingSpinner= (isLoading) =>{
+    const loadingSpinner= document.getElementById('loading-spinner');
+    if(isLoading){
+        loadingSpinner.classList.remove('hidden');
+    }
+    else{
+        loadingSpinner.classList.add('hidden');
+    }
+
 }
 
 loadPhone();
